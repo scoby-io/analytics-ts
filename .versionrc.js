4320edeff523a7e03d616fdaadb7cdce627fa811
Peter Viergutz <viergutz@gmail.com>
2023-04-23 14:09:51 +0200

    fix: build pipeline
    Signed-off-by: Peter Viergutz <viergutz@gmail.com>

---
+++ .versionrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  releaseCommitMessageFormat: 'chore(release): {{currentTag}} [skip ci]',
+  // bumpFiles: [
+  //   {
+  //     filename: 'composer.json',
+  //     type: 'json',
+  //   },
+  // ],
+  // skip: {
+  //   changelog: true,
+  // },
+};
